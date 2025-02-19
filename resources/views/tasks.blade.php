774029cecaa5725a73cc08b5ef95984fd61f9dee
Arthur <artcosta1@gmail.com>
2025-02-18 21:23:59 -0300

    feat: update Docker configuration and update css to using tailwind

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -14,67 +14,67 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     </head>
 
-    <body>
-        <header>
-            <div id="div-header" class="container mx-auto">
-                <div id="div-2-header">
+    <body class="min-h-screen w-full bg-[#191919]">
+        <header class="h-[200px] w-full bg-[#0D0D0D]">
+            <div class="flex place-content-center h-full">
+                <div class="flex items-center gap-3">
                     <div>
                         <img src="{{ asset('images/rocket.svg') }}" alt="Logo">
                     </div>
-                    <div id="div-3-header">
-                        <span style="color: #4EA8DE;font-size: 36px; font-weight: 720">to</span><span style="color: #5E60CE;font-size: 36px;font-weight: 720">do</span>
+                    <div> 
+                        <span class="text-[#4EA8DE] text-4xl font-bold">to</span><span class="text-[#5E60CE] text-4xl font-[900]">do</span>
                     </div>
                 </div>
             </div>
         </header>
 
         <main>
-            <div id="div-1-main">
+            <div class="flex gap-8 -mt-10 justify-center">
                 <form action="{{ url('/tasks') }}" method="POST">
-                    <input id="input-task" required maxlength="105"  type="text" name="task" placeholder="Digite sua tarefa">
-                    <button id="button-task" type="submit">Adicionar <i class="fa-solid fa-plus fa-sm"></i></button>
+                    <input class="w-[880px] h-[50px] p-5 text-sm border-none rounded-sm my-5 bg-[#262626] text-white" required  maxlength="105" type="text" name="task" placeholder="Digite sua tarefa">
+                    <button class="h-[50px] p-2 text-sm border-none rounded my-5 bg-[#1E6F9F] text-white" type="submit">Adicionar <i class="fa-solid fa-plus fa-sm"></i></button>
                     @csrf 
                 </form>
             </div>
 
-            <section>
-                <div id="div-1-section">
-                    <div id="div-2-section">
-                        <span id="created-tasks" style="color: #4EA8DE; font-weight: 720;">Tarefas criadas</span>
-                        <span style="color: white; font-weight: bold; font-size: 14px; background-color: #262626; padding: 3px 8px 3px 8px; border-radius: 15px">{{ $totalTasks }}</span>
+            <section class="w-[965px] mx-auto mt-10">
+                <div class="flex justify-between items-center w-full">
+                    <div class="flex gap-2">
+                        <span id="created-tasks" class="text-[#4EA8DE] font-bold">Tarefas criadas</span>
+                        <span class="text-white font-bold text-sm bg-[#262626] py-0.5 px-2 rounded-[15px]">{{ $totalTasks }}</span>
                     </div>
-                    <div id="div-2-section">
-                        <span style="color: #5E60CE; font-weight: 720;">Concluídas</span>
-                        <span style="color: white; font-weight: bold; font-size: 14px; background-color: #262626; padding: 3px 8px 3px 8px; border-radius: 15px">{{ $totalCompleted }} de {{ $totalTasks }}</span>
+                    <div class="flex gap-2">
+                        <span class="text-[#5E60CE] font-bold">Concluídas</span>
+                        <span class="text-white font-bold text-sm bg-[#262626] py-0.5 px-2 rounded-[15px]">{{ $totalCompleted }} de {{ $totalTasks }}</span>
                     </div>
                 </div>
                 
-                <hr style="border-color: #333333; margin-top: 20px">
+                <hr class="border-[#333333] mt-5">
             </section>
 
-            <section> 
+            <section class="w-[965px] mx-auto mt-10"> 
                 @forelse ($tasks as $task)
-                <div id="section-tasks-div-1">
-                    <div id="section-tasks-div-2">
-                        <input id="checkbox" type="checkbox" name="completed" id="completed-{{ $task->id }}" {{ $task->completed ? 'checked' : '' }}
+                <div class="flex justify-between items-center gap-2 text-white h-[50px] p-[15px_20px] text-base rounded my-5 bg-[#262626]">
+                    <div class="flex items-center gap-2">
+                        <input class="w-5 h-5 rounded mr-5 bg-transparent" type="checkbox" name="completed" id="completed-{{ $task->id }}" {{ $task->completed ? 'checked' : '' }}
                         onchange="updateTask({{ $task->id }}, this.checked)">
                         <p style="{{ $task->completed ? 'text-decoration: line-through; color: #808080;' : '' }}">
                             {{ $task->task }}
                         </p>
                     </div>
-                    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" onsubmit="return confirm('Deseja excluir essa tarefa?')"  style="margin: 0;">
+                    <form class="m-0 pt-1" action="{{ route('tasks.destroy', $task->id) }}" method="POST" onsubmit="return confirm('Deseja excluir essa tarefa?')">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" style="border: none; background: none; cursor: pointer;">
-                            <img src="{{ asset('images/trash.svg') }}" alt="Lixeira para deletar tarefa" style="width: 30px; height: 30px;">
+                        <button class="border-none bg-none cursor-pointer" type="submit">
+                            <img class="w-[30px] h-[30px]" src="{{ asset('images/trash.svg') }}" alt="Lixeira para deletar tarefa">
                         </button>
                     </form>
                 </div>
                     @empty
-                    <div id="empty-div">
-                        <img src="{{ asset('images/clipboard.svg') }}" alt="Ícone prancheta" style="width: 50px; height: 70px;">
-                        <p style="font-weight: bold">Você ainda não tem tarefas cadastradas</p>
-                        <p>Crie tarefas e organize seus itens a fazer</p>
+                    <div class="flex flex-col items-center text-center">
+                        <img class="w-[70px] h-[50px]" src="{{ asset('images/clipboard.svg') }}" alt="Ícone prancheta">
+                        <p class="font-bold text-[#7F7F7F]">Você ainda não tem tarefas cadastradas</p>
+                        <p class="text-[#7F7F7F]">Crie tarefas e organize seus itens a fazer</p>
                     </div>
                 @endforelse
 
